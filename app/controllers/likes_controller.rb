bf333509fba3420e071ec8c2d8ad0e2f62b152ed
fumi238000 <s21231128@gmail.com>
2020-10-20 10:08:15 +0900

    いいね機能の非同期設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,14 @@
 class LikesController < ApplicationController
   
   def create
-    current_user.likes.create!(post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    current_user.likes.create!(post_id: @post.id)
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    current_user.likes.find_by(post_id: params[:post_id]).destroy!
+    @post = Post.find(params[:post_id])
+    current_user.likes.find_by(post_id: @post.id).destroy!
     redirect_back(fallback_location: root_path)
   end
 end

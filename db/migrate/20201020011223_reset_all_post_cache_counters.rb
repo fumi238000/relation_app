a9feb66e270ab42cce095779d6b482751161fce0
fumi238000 <s21231128@gmail.com>
2020-10-20 10:14:57 +0900

    likes_countカラムの追加

---
+++ db/migrate/20201020011223_reset_all_post_cache_counters.rb
@@ -0,0 +1,11 @@
+class ResetAllPostCacheCounters < ActiveRecord::Migration[6.0]
+  def change
+  end
+
+  def up
+    Post.find_each { |post| Post.reset_counters(post.id, :likes_count) }
+  end
+
+  def down
+  end
+end

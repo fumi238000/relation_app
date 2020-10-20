c4b7b6074a65f57df06768f657433d804d73d74d
fumi238000 <s21231128@gmail.com>
2020-10-20 09:23:30 +0900

    中間テーブルとの関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  validates :content, presence: true
+  has_many :liked_users, through: :likes, source: :user
+
+validates :content, presence: true
 end

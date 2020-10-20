700e38e8045773faae49041a57a7d840fde5abb7
fumi238000 <s21231128@gmail.com>
2020-10-20 09:12:52 +0900

    likesテーブルの関連付け

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,34 @@
-# 各テーブルのデータを全て削除
+# ログイン時に使用する「メールアドレス」と「パスワード」を定数化して上に配置
+EMAIL = "test@example.com"
+PASSWORD = "password"
+
+# データを全削除
 User.destroy_all
 Post.destroy_all
 
-# 初期データを追加(userテーブル)
+# 以下，開発用の初期データ
 user1 = User.create!(email: "satou@example.com", password: "password")
 user2 = User.create!(email: "suzuki@example.com", password: "password")
 user3 = User.create!(email: "tanaka@example.com", password: "password")
 
-# 初期データを追加(postテーブル)
-Post.create!(content: "おはよう", user_id: user2.id)
-Post.create!(content: "こんにちは", user_id: user3.id)
-Post.create!(content: "こんばんは", user_id: user3.id)
+# ***** 以下を編集(変数に代入) *****
+post1 = user2.posts.create!(content: "沖縄そば")
+post2 = user1.posts.create!(content: "海ぶどう")
+post3 = user3.posts.create!(content: "ゴーヤーチャンプルー")
+post4 = user3.posts.create!(content: "サーターアンダギー")
+post5 = user1.posts.create!(content: "タコライス")
 
+# ***** 以下を追加 *****
+post1.likes.create!(user_id: user1.id)
+post1.likes.create!(user_id: user3.id)
+post2.likes.create!(user_id: user2.id)
+post3.likes.create!(user_id: user1.id)
+post4.likes.create!(user_id: user1.id)
+post4.likes.create!(user_id: user2.id)
+post5.likes.create!(user_id: user3.id)
+# ***** 以上を編集・追加 *****
 
-puts "初期データのと投入に成功しました！"
+# ログイン時に使用するアカウント（変数への代入は不要）
+User.create!(email: EMAIL, password: PASSWORD)
+
+puts "初期データの投入に成功しました！"
